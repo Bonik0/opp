f859c59d26a92edc1b9a566d364141a38bf52895
Bonik0 <115299174+Bonik0@users.noreply.github.com>
2023-11-10 15:23:00 +0000

    Add files via upload

--- lab1/conversion_time.cpp
+++ lab1/conversion_time.cpp
@@ -1,17 +1,17 @@
 #include "conversion_time.h"
 
 std::string conversion_time(int hour, int minute, std::string period){
+    if (!(0 < hour <= 12 and 0 <= minute <= 60 and (period == "am" or period == "pm"))){
+        throw std::invalid_argument("Invalid arguments!!");
+    }
     std::string ans = std::string(4, '0');
-
     if (period == "pm") {
         hour = (hour + 12) % 24;
     }
-
     ans[0] = int(hour / 10) + '0';
     ans[1] = hour % 10 + '0';
     ans[2] = int(minute / 10) + '0';
     ans[3] = minute % 10 + '0';
     
     return ans;
-
 }
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,26 +3,11 @@
 
 int main()
 {
-    std::string time;
-    std::string period;
-    std::cout << "Input the time in the format: HH:MM am or pm: ";
-    std::cin >> time;
-    std::cin >> period;
+    std::cout << "Input the time in the format: HH MM am or pm: ";
     int hour;
+    std::string period;
     int minute;
-    try {
-        hour = stoi(time.substr(0, time.find(':')));
-        minute = stoi(time.substr(time.find(':') + 1, 2));
-    } catch (std::invalid_argument){
-        std::cout << "Incorrect input";
-        return 1;
-    }
-    
-    if (0 <= hour <= 12 and 0 <= minute <= 60 and (period == "am" or period == "pm")){
-        std::cout << conversion_time(hour, minute, period);
-    } else {
-        std::cout << "Incorrect input";
-    }
-
-    
+    std::cin >> hour >> minute;
+    std::cin >> period;
+    std::cout << conversion_time(hour, minute, period);
 }
